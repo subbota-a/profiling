f488bf780a426dfd98f6f183931a0e4d510f3e09
Alexey Subbota <subbota-job@yandex.ru>
2023-12-22 09:38:57 +0500

    вынес реализацию в отдельный файл

---
+++ SingleNumber/code.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+class Solution1 {
+public:
+  static int singleNumber(std::vector<int> &nums);
+};
+
+class Solution2 {
+public:
+  static int singleNumber(std::vector<int> &nums);
+};
+
+class Solution3 {
+public:
+  static int singleNumber(std::vector<int> &nums);
+};
+
+class Solution4 {
+public:
+  static int singleNumber(std::vector<int> &nums);
+};
